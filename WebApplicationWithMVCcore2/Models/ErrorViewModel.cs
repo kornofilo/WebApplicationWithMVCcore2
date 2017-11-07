7404e55ae556c322db4092719ddda2928dfb3afb
Ricardo Rubio <ricardo.rubio@utp.ac.pa>
2017-11-06 20:25:17 -0500

    Agregar archivos de proyecto.

---
+++ WebApplicationWithMVCcore2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplicationWithMVCcore2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
