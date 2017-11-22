efd769613e26f8eceef9384891cf0dece22d8a23
Ricardo Rubio <ricardo.rubio@utp.ac.pa>
2017-11-22 14:09:56 -0500

    actualizacion de todos los cambios

--- WebApplicationWithMVCcore2/Controllers/HomeController.cs
+++ WebApplicationWithMVCcore2/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace WebApplicationWithMVCcore2.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Acerca de nosotros.";
 
             return View();
         }
